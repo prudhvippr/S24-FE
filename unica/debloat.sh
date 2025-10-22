163075da33db12938b596e744805fb875a2fedba
prudhvippr <ppr.pulikanti@gmail.com>
2025-10-22 23:57:40 +0530

    Update debloat.sh

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -170,9 +170,6 @@ system/priv-app/AppUpdateCenter
 system/priv-app/AutoDoodle
 system/priv-app/AvatarEmojiSticker
 system/priv-app/BeaconManager
-system/priv-app/Bixby
-system/priv-app/BixbyInterpreter
-system/priv-app/BixbyVisionFramework3.5
 system/priv-app/DynamicLockscreen
 system/priv-app/FBInstaller_NS
 system/priv-app/FBServices
@@ -187,7 +184,6 @@ system/priv-app/StoryService
 system/priv-app/StickerFaceARAvatar
 system/priv-app/SmartSwitchAssistant
 system/app/SmartManager_v6_DeviceSecurity
-system/priv-app/SettingsBixby
 system/priv-app/SamsungSmartSuggestions
 system/priv-app/SamsungPass
 system/priv-app/SamsungMagnifier3
@@ -212,7 +208,6 @@ system/app/ARCore
 system/app/ARDrawing
 system/app/AASAservice
 system/app/BasicDreams
-system/app/BixbyWakeup
 system/app/BlockchainBasicKit
 system/app/CompanionDeviceManager
 system/app/EasterEgg
@@ -237,7 +232,6 @@ system/app/SmartReminder
 system/app/VisionIntelligence3.7
 system/app/VoiceAccess
 system/app/BBCAgent
-system/priv-app/DeXonPC
 system/priv-app/GameHome
 system/priv-app/GameOptimizingService
 system/priv-app/NSFusedLocation_v6.0
