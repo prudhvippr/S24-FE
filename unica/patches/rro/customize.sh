4021fba1580e4f0fc57f2f9353d0862a3f1a2d58
prudhvippr <ppr.pulikanti@gmail.com>
2025-10-23 11:00:04 +0530

    Update customize.sh

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,5 +1,5 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "product" "overlay/framework-res__g0sxxx__auto_generated_rro_product.apk"
+    DECODE_APK "product" "overlay/framework-res__b0sxxx__auto_generated_rro_product.apk"
 
     LOG "- Applying stock overlay configs"
     rm -rf "$APKTOOL_DIR/product/overlay/framework-res__g0sxxx__auto_generated_rro_product.apk/res"
